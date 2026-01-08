5d847e2c110a8e4a172690150e6fefbef80e9750
shubhashree500 <Shreeshubha98>
2026-01-08 13:54:19 +0530

    .

---
+++ app/services/page.tsx
@@ -0,0 +1,11 @@
+// app/contact/page.tsx
+import React from "react";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Reach out to us at contact@resmed.com or call 123-456-7890.</p>
+    </div>
+  );
+}
